c1172f8c1b207e77abf7b6f123a3aa417d0249a7
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2024-03-19 10:58:43 -0300

    Add files via upload

---
+++ scripts/Ciclovias en Ejecucion (DAMI II).js
@@ -0,0 +1,8 @@
+var CicloviasenEjecucionDami ={
+"type": "FeatureCollection",
+"name": "Ciclovias en Ejecucion (DAMI II)",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Distancia": "Frente a Cerveceria (0,16 Km)", "Id": "1", "TIPO_2": "Ciclovia: CERVANTES SUR" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -68.846905054244218, -32.953033586624223 ], [ -68.846034866446999, -32.951787660700603 ] ] ] } }
+]
+}
---
+++ scripts/EstacionesdeServicio.js
@@ -0,0 +1,26 @@
+var EstacionesdeServicio ={
+"type": "FeatureCollection",
+"name": "Estaciones de Servicio",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "Nombre": "Axion", "Direccion": "Carril Sarmiento y Alberti" }, "geometry": { "type": "Point", "coordinates": [ -68.825730034191238, -32.94193175384423 ] } },
+{ "type": "Feature", "properties": { "id": 2, "Nombre": "YPF", "Direccion": "Carril Rodriguez Peña e Independencia" }, "geometry": { "type": "Point", "coordinates": [ -68.821081427566483, -32.92726781400949 ] } },
+{ "type": "Feature", "properties": { "id": 3, "Nombre": "Copec", "Direccion": "Carril Sarmiento y Alem" }, "geometry": { "type": "Point", "coordinates": [ -68.834452100957918, -32.940062402615254 ] } },
+{ "type": "Feature", "properties": { "id": 4, "Nombre": "YPF", "Direccion": "Carril Sarmiento y Cervantes" }, "geometry": { "type": "Point", "coordinates": [ -68.84015217812879, -32.937969080424374 ] } },
+{ "type": "Feature", "properties": { "id": 5, "Nombre": "YPF", "Direccion": "Av. San Martín Sur y Río Plomo" }, "geometry": { "type": "Point", "coordinates": [ -68.854285760600632, -32.949329153656151 ] } },
+{ "type": "Feature", "properties": { "id": 6, "Nombre": "YPF", "Direccion": "Av. San Martín y Hualpa" }, "geometry": { "type": "Point", "coordinates": [ -68.84988025712849, -32.933842005327122 ] } },
+{ "type": "Feature", "properties": { "id": 7, "Nombre": "YPF", "Direccion": "Av. San Martín y French" }, "geometry": { "type": "Point", "coordinates": [ -68.850062090380789, -32.930452948786623 ] } },
+{ "type": "Feature", "properties": { "id": 8, "Nombre": "Shell", "Direccion": "Carril Rodriguez Peña y Primitivo de la Reta" }, "geometry": { "type": "Point", "coordinates": [ -68.826823010153703, -32.925218981818048 ] } },
+{ "type": "Feature", "properties": { "id": 9, "Nombre": "Copec", "Direccion": "Tiburcio Benegas y J.V.González" }, "geometry": { "type": "Point", "coordinates": [ -68.858390843916908, -32.937947516625776 ] } },
+{ "type": "Feature", "properties": { "id": 10, "Nombre": "Civit", "Direccion": "J.V.González y Civit" }, "geometry": { "type": "Point", "coordinates": [ -68.858596394550005, -32.930668604808737 ] } },
+{ "type": "Feature", "properties": { "id": 11, "Nombre": "YPF", "Direccion": "Av. San Martín y Chacabuco" }, "geometry": { "type": "Point", "coordinates": [ -68.846595399896231, -32.91738321264706 ] } },
+{ "type": "Feature", "properties": { "id": 12, "Nombre": "Shell", "Direccion": "Av. San Martín y Pasteur" }, "geometry": { "type": "Point", "coordinates": [ -68.845381860581767, -32.912787276785146 ] } },
+{ "type": "Feature", "properties": { "id": 13, "Nombre": "Servicio Oil", "Direccion": "Minuzzi 439" }, "geometry": { "type": "Point", "coordinates": [ -68.838497890822367, -32.908962678940838 ] } },
+{ "type": "Feature", "properties": { "id": 14, "Nombre": "YPF", "Direccion": "Beltrán y Arizu" }, "geometry": { "type": "Point", "coordinates": [ -68.851589884990133, -32.908289001642054 ] } },
+{ "type": "Feature", "properties": { "id": 15, "Nombre": "Delta GNC", "Direccion": "Brasil y Los Alamos" }, "geometry": { "type": "Point", "coordinates": [ -68.839286493731962, -32.904509012766447 ] } },
+{ "type": "Feature", "properties": { "id": 16, "Nombre": "YPF", "Direccion": "Perito Moreno y Mansilla" }, "geometry": { "type": "Point", "coordinates": [ -68.847494683915954, -32.940135386013338 ] } },
+{ "type": "Feature", "properties": { "id": 17, "Nombre": "GNC Corredor", "Direccion": "Amengual y Corredor del Oeste" }, "geometry": { "type": "Point", "coordinates": [ -68.864357741621163, -32.908023511643329 ] } },
+{ "type": "Feature", "properties": { "id": 18, "Nombre": "GNC", "Direccion": "Paso de los Andes y Pellegrini" }, "geometry": { "type": "Point", "coordinates": [ -68.860519478838313, -32.90842672426534 ] } },
+{ "type": "Feature", "properties": { "id": 19, "Nombre": "Petrosur", "Direccion": "Paso de los Andes y Maza" }, "geometry": { "type": "Point", "coordinates": [ -68.859780287138648, -32.923007516990218 ] } }
+]
+}
